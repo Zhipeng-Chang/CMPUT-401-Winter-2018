bcbcb967063546bc7f3a39bd76e2a51c873496b7
Danissss <danis.cao@hotmail.com>
2018-02-01 11:40:40 -0700

    Update 401project.sh

--- 401project.sh
+++ 401project.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y docker-ce
 sudo docker pull cassandra
 
 # run cassandra
-sudo docker run cassandra
+sudo docker run cassandra 
 
 
 # running a docker container
@@ -27,7 +27,7 @@ sudo docker container start [containerName]
 sudo docker exec -it [container_name or container_id] bash
 
 # in the container
-apt-get update
+sudo apt-get update
 
 # configure the cassandra
 
@@ -38,38 +38,38 @@ apt-get update
 		tar xfvz ycsb-0.12.0.tar.gz
         rm ycsb-0.12.0.tar.gz
 		cd ycsb-0.12.0
-        # run ycsb
-        ./bin/ycsb
+#         # run ycsb
+#         ./bin/ycsb
 
-        # create a cassandra table: run cassandra query language
-        cqlsh
-        # create keyspace, table, data with cassandra
-        > create keyspace testKeyspace with replication = {'class':'SimpleStrategy','replication_factor': 3} ;
-        >create table test (testUsr text, testpass text, primary key(testUsr));
-        >insert into test (testUsr,testpass) values ('xcao','2333');
-        # to use ycsb to against cassandra to test performance, create the ycsb keyspaces;
-        https://github.com/brianfrankcooper/YCSB/tree/master/cassandra
-        //cqlsh> create keyspace ycsb
-        WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 3 };
-        cqlsh> USE ycsb;
-        cqlsh> create table usertable (
-        y_id varchar primary key,
-        field0 varchar,
-        field1 varchar,
-        field2 varchar,
-        field3 varchar,
-        field4 varchar,
-        field5 varchar,
-        field6 varchar,
-        field7 varchar,
-        field8 varchar,
-        field9 varchar);
+#         # create a cassandra table: run cassandra query language
+#         cqlsh
+#         # create keyspace, table, data with cassandra
+#         > create keyspace testKeyspace with replication = {'class':'SimpleStrategy','replication_factor': 3} ;
+#         >create table test (testUsr text, testpass text, primary key(testUsr));
+#         >insert into test (testUsr,testpass) values ('xcao','2333');
+#         # to use ycsb to against cassandra to test performance, create the ycsb keyspaces;
+#         https://github.com/brianfrankcooper/YCSB/tree/master/cassandra
+#         //cqlsh> create keyspace ycsb
+#         WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 3 };
+#         cqlsh> USE ycsb;
+#         cqlsh> create table usertable (
+#         y_id varchar primary key,
+#         field0 varchar,
+#         field1 varchar,
+#         field2 varchar,
+#         field3 varchar,
+#         field4 varchar,
+#         field5 varchar,
+#         field6 varchar,
+#         field7 varchar,
+#         field8 varchar,
+#         field9 varchar);
 
 
 
 
     #  cassandra configuration file is in cassandra.yaml
-    cd /etc/cassandra/cassandra.yaml
+    #  cd /etc/cassandra/cassandra.yaml
     # http://docs.datastax.com/en/cassandra/2.1/cassandra/configuration/configCassandra_yaml_r.html#configCassandra_yaml_r__PerformanceTuningProps
     # this contain the main factors that could optimize the performance of cassandra
 
