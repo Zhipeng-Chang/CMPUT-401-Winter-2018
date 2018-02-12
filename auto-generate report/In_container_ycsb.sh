f192d01016edd0ee4f3f4b4f14fd62de98f3c432
Danissss <danis.cao@hotmail.com>
2018-02-12 11:47:19 -0700

    auto generate report ycsb

---
+++ auto-generate report/In_container_ycsb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# this program automatically run five different workloads provided from ycsb
+# the output files are named respectively: workloada.txt, workloadb.txt, workloadc.txt
+# workloadd.txt, workloade.txt
+# at the end the python script will output a nice table with all information
+# cql file will clean all the database for next iteration
+
+# required file:
+# output.py
+
+
+# set_up the ycsb
+cqlsh -e "create keyspace if not exists ycsb WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 1 };"
+cqlsh -e "create table ycsb.usertable (y_id varchar primary key, field0 varchar, field1 varchar, field2 varchar, field3 varchar, field4 varchar, field5 varchar, field6 varchar, field7 varchar, field8 varchar, field9 varchar) with compression = {'sstable_compression': ''};"
+
+
+# step 1. get the ip address
+# make the output as variable
+# OUTPUT="$(ls -1)"
+# echo "${OUTPUT}"
+nodetool status ycsb > hostIP.txt
+IP_address = "$(python getHostIp.py)"
+
+
+# step 2. generate five report
+declare -a arr=("a" "b" "c" "d" "e")
+for i in "${arr[@]}"
+do
+./ycsb-0.12.0/bin/ycsb load cassandra-cql -p hosts=${IP_address} -P workloads/workload${i} -s > ../workload_load_output${i}.txt
+./ycsb-0.12.0/bin/ycsb run  cassandra-cql -p hosts=${IP_address} -P workloads/workload${i} -s > ../workload_run_output${i}.txt
+# or do whatever with individual element of the array
+done
+
+
+# give the general report via python file
+python generate_report.py
+
+# clean up the ycsb
+cqlsh -e "drop table ycsb.usertable;"
+
+echo "report generated finished"
+exit 1
+
---
+++ auto-generate report/generate_report.py
@@ -0,0 +1,45 @@
+#
+#
+#
+#
+#
+# note: this script only write the run output
+
+import matplotlib.pyplot as plt
+
+file = open("all_result.txt", 'w')
+
+fileOrder = ['a','b','c','d','e']
+for i in range(0,5):
+	
+	temp_file_name = "workload_run_output"+fileOrder[i]+".txt"
+
+	files = open(temp_file_name,"r")
+	Line = files.readlines()
+
+	
+	OVERALL = '[OVERALL]'
+	properties_OVERALL_index = None
+
+	for i in Line:
+		if OVERALL in i:
+			properties_OVERALL_index = Line.index(i)
+
+
+	End_Indx = len(Line)
+	
+
+	test_general_info = []
+	file.write("========================================================\n")
+	write_string = "test_output of "+ temp_file_name + '\n'
+	file.write(write_string)
+	file.write("========================================================\n")
+	if properties_OVERALL_index != None:
+		for i in range(properties_OVERALL_index,End_Indx-1):
+			file.write(Line[i]+"\n")
+
+
+			temp_line = Line[i]
+			temp_line_split = temp_line.split(",")
+
+			test_general_info.append(temp_line_split)
