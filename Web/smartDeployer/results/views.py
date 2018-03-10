043721c56ee3df5e25d32d714434e096f97cba07
anicn <nanic@ualberta.ca>
2018-03-10 12:24:53 -0700

    rename home page html

--- Web/smartDeployer/results/views.py
+++ Web/smartDeployer/results/views.py
@@ -15,7 +15,7 @@ def index(request):
     View function for home page of site.
     """
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'sample_page.html',context={})
+    return render(request, 'home_page.html',context={})
 
 def team(request):
     return render(request, 'team.html',context={})
