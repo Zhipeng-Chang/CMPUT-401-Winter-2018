07e6d8e100c322d05ea0474e1b485c47755c42b4
anicn <nanic@ualberta.ca>
2018-03-21 10:36:05 -0600

    first test passes

--- Web/smartDeployer/results/test_models.py
+++ Web/smartDeployer/results/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from .models import WorkloadA
 
-class YourTestClass(TestCase):
+class WorkloadATest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
@@ -11,14 +12,6 @@ class YourTestClass(TestCase):
         print("setUp: Run once for every test method to setup clean data.")
         pass
 
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
-
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
-
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1, 2)
+    def test_string_representation(self):
+        workloadA = WorkloadA(workload=10)
+        self.assertEqual(workloadA.workload, workloadA.workload)
